1790cb4804dc7c1ec6a4573bc23893b921a5f590
JClerve <joesonclerve9@gmail.com>
2024-10-09 20:42:34 +0530

    hii this is my portfolio

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,40 +1,39 @@
-import React from 'react'
-import styled, { keyframes, ThemeProvider } from 'styled-components'
-import {DarkTheme} from './Themes';
+import React from "react";
+import styled, { keyframes, ThemeProvider } from "styled-components";
+import { DarkTheme } from "./Themes";
 
-
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-import PowerButton from '../subComponents/PowerButton';
-import ParticleComponent from '../subComponents/ParticleComponent';
-import BigTitle from '../subComponents/BigTitlte'
-import astronaut from '../assets/Images/spaceman.png'
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import ParticleComponent from "../subComponents/ParticleComponent";
+import BigTitle from "../subComponents/BigTitlte";
+import astronaut from "../assets/Images/spaceman.png";
 
 const Box = styled.div`
-background-color: ${props => props.theme.body};
-width: 100vw;
-height:100vh;
-position: relative;
-overflow: hidden;
-`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
 const float = keyframes`
 0% { transform: translateY(-10px) }
 50% { transform: translateY(15px) translateX(15px) }
 100% { transform: translateY(-10px) }
 
-`
+`;
 const Spaceman = styled.div`
-position: absolute;
-top: 10%;
-right: 5%;
-width: 20vw;
-animation: ${float} 4s ease infinite;
-img{
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  width: 20vw;
+  animation: ${float} 4s ease infinite;
+  img {
     width: 100%;
     height: auto;
-}
-`
-const Main =  styled.div`
+  }
+`;
+const Main = styled.div`
   border: 2px solid ${(props) => props.theme.text};
   color: ${(props) => props.theme.text};
   padding: 1rem;
@@ -46,46 +45,40 @@ const Main =  styled.div`
   justify-content: center;
   align-items: center;
   font-size: calc(0.6rem + 1vw);
- backdrop-filter: blur(4px);
-  
+  backdrop-filter: blur(4px);
+
   position: absolute;
   left: calc(5rem + 5vw);
   top: 10rem;
-  font-family: 'Ubuntu Mono', monospace;
+  font-family: "Ubuntu Mono", monospace;
   font-style: italic;
-`
-
-
-
+`;
 
 const AboutPage = () => {
-    return (
-        <ThemeProvider theme={DarkTheme}>
-<Box>
-
-<LogoComponent theme='dark'/>
-<SocialIcons theme='dark'/>
-<PowerButton />
-<ParticleComponent theme='dark' />
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Box>
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+        <ParticleComponent theme="dark" />
 
         <Spaceman>
-            <img src={astronaut} alt="spaceman" />
-        </Spaceman>    
+          <img src={astronaut} alt="spaceman" />
+        </Spaceman>
         <Main>
-  I'm a Full-Stack MERN Developer from India with strong OOP principles and experience in Spring Boot. I love building sleek, efficient apps across the full stack and enjoy experimenting with new technologies. I'm also a freelancer, blogger, and a constant learner.
-  <br /> <br/>
-  To me, coding is an art. Let's connect via social links!
-</Main>
-
+          I'm a Full-Stack MERN Developer from Sri Lanka with strong OOP
+          principles and experience in Spring Boot. I love building sleek,
+          efficient apps across the full stack and enjoy experimenting with new
+          technologies. I'm also a freelancer, blogger, and a constant learner.
+          <br /> <br />
+          To me, coding is an art. Let's connect via social links!
+        </Main>
 
         <BigTitle text="ABOUT" top="10%" left="5%" />
+      </Box>
+    </ThemeProvider>
+  );
+};
 
-
-        </Box>
-
-        </ThemeProvider>
-        
-    )
-}
-
-export default AboutPage
+export default AboutPage;
