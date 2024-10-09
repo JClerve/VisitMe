247bf489dc6db6e147cd6fdb6cc0c667684fce72
JClerve <joesonclerve9@gmail.com>
2024-10-09 18:44:01 +0530

    Initial commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,35 @@
+// src/data/WorkData.js
+
+export const projectsData = [
+    {
+      id: 1,
+      name: "Romodo - Transportion Platform",
+      description: "This platform built with TypeScript, Material UI, NodeJS, ExpressJS, MongoDB, Firebase, and Google Cloud Platform.",
+      technologies: [
+        "TypeScript",
+        "Material UI",
+        "NodeJS",
+        "ExpressJS",
+        "MongoDB",
+        "Firebase",
+        "Google Cloud Platform",
+      ],
+      github: "https://github.com/orgs/Romodo-Copy/repositories",
+    },
+    {
+      id: 2,
+      name: "CraftedByJoe - Portfolio Web App",
+      description: "This portfolio website showcases my skills and expertise, built using HTML, JavaScript, CSS, and powered by AWS for robust cloud integration.",
+      technologies: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "EmailJS",
+        "AWS",
+        "EC2",
+        "S3 Bucket",
+      ],
+      github: "https://github.com/JClerve/Customized-Tea-Maker",
+    },
+    // Add more projects here
+  ];
